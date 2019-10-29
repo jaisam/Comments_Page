cf462b6298070263750ff6b223b639291e6d9164
jaisam <jaisamtani123@gmail.com>
2019-10-29 05:34:28 +0530

    Commit #10

--- Comment-Page-Backend/Routes/userRoute.js
+++ Comment-Page-Backend/Routes/userRoute.js
@@ -4,6 +4,29 @@ const mongoose = require('mongoose');
 const User = require('../Models/User');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const checkAuthorization = require('./middlewares/check-auth');
+
+
+//[start] Get Logged In User data
+router.get('/', checkAuthorization, async (req, res, next) => {
+    try {
+        console.log('req.body : ' , req.body);
+        console.log('eq.userData ', req.userData.userId);
+        const user = await User.findOne({ _id: req.userData.userId });
+        console.log('user', user);
+
+        if (user) {
+            console.log('Inside get user', user);
+            res.json(user);
+        } else { 
+            res.status(404).json({ msg :`User with ${req.userData.userId} does not exist` });
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ msg : error.msg });
+    }
+});
+//[end] Get Logged In User data
 
 
 router.post('/signup', (req, res, next) => {
@@ -22,9 +45,9 @@ router.post('/signup', (req, res, next) => {
                     } else {
                         // Gneerating hash for password
                         const newUser = new User({
-                            firstName : req.body.firstName,
-                            lastName : req.body.lastName,
-                            company : req.body.company,
+                            firstName: req.body.firstName,
+                            lastName: req.body.lastName,
+                            company: req.body.company,
                             email: req.body.email,
                             password: hash
                         });
@@ -75,7 +98,7 @@ router.post('/login', (req, res, next) => {
                         );
                         return res.json({
                             msg: "Authentication successful",
-                            email : user[0].email,
+                            email: user[0].email,
                             token: token
                         });
                     } else {
