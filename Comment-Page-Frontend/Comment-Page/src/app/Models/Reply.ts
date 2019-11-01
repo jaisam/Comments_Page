4f54560b60e9cec9afd046a73e213c912b37ba6a
jaisam <jaisamtani123@gmail.com>
2019-11-01 19:41:35 +0530

    Removing extra folder and adding only angular project folder

--- Comment-Page-Frontend/Comment-Page/src/app/Models/Reply.ts
+++
@@ -1,9 +0,0 @@
-export interface Comment {
-    userName: String,
-    userImage: String,
-    description: String,
-    upvote : Number,
-    downvote : Number,
-    replies: [String],
-    creationDate: Date
-};
